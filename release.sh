d65b064edea3cf655ed14ec982ae2d607c4d87ec
Luke Plant <L.Plant.98@cantab.net>
2021-04-06 22:10:28 +0300

    Corrected release script

--- release.sh
+++ release.sh
@@ -9,6 +9,6 @@ find . -type d | xargs chmod ugo+rx
 
 VERSION=$(./setup.py --version) || exit 1
 
-twine upload dist/table-format-$VERSION.tar.gz dist/table_format-$VERSION-py2.py3-none-any.whl || exit 1
+twine upload dist/table-format-$VERSION.tar.gz dist/table_format-$VERSION-py3-none-any.whl || exit 1
 
 git push
